fa63dac1f294f4659b5af646ec3d55b689b155da
Pulkit Mahaur <pulkitmahour15jan@gmail.com>
2021-09-20 12:54:13 +0530

    remove waste

--- q_and_a_server.py
+++ q_and_a_server.py
@@ -1,38 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
-
-def making_page():
-	var =  "<html>" + \
-	"<head>"+ \
-		"<title>Q and A server</title>" +\
-	"</head>"+ \
-	"<body bgcolor = #5B3E8A" + \
-		"<h1><font size = 10> Type the following in the URL for getting the result</h1>"+\
-			"<ol>" +\
-				"<font size = 6, color = black>" "<i>" "<li>" "For check the prime number" "</li>" "</i>" +\
-					"<ul>" +\
-						"<li>" "<font size = 5, color = blue>" "/prime/number" "</li>" +\
-					"</ul>" +\
-						"<p>&nbsp;</p>"+\
-				"<font size = 6, color = black>" "<i>" "<li>" "For check the nth prime number" "</li>" "</i>" +\
-					"<ul>" +\
-						"<li>" "<font size = 5, color = blue>" "/nth_prime/number" "</li>" +\
-					"</ul>" +\
-						"<p>&nbsp;</p>"+\
-				"<font size = 6, color = black>" "<i>" "<li>" "iska pata nahi" "</li>" "</i>" +\
-					"<ul>" +\
-						"<li>" "<font size = 5, color = blue>" "For check the prime number" "</li>" +\
-					"</ul>" +\
-						"<p>&nbsp;</p>"+\
-				"<font size = 6, color = black>" "<i>" "<li>" "For check the nth sum" "</li>" "</i>" +\
-					"<ul>" +\
-						"<li>" "<font size = 5, color = blue>" "/nth_sum/number" "</li>" +\
-					"</ul>" +\
-			"</ol>" +\
-	"</body>" + \
-	"</html>"
-
-	return var
+from server_utilities import *
 
 
 class handler(BaseHTTPRequestHandler):
@@ -40,10 +8,75 @@ class handler(BaseHTTPRequestHandler):
 		self.send_response(200)
 		self.send_header('Content-type','text/html')
 		self.end_headers()
-		viewer = making_page()
-		self.wfile.write(bytes(viewer,'utf-8'))
-		if "prime" in self.path[1:]:
-			print("haa sahi hai")
+
+		if self.path[1:] == '':
+			self.wfile.write(bytes(question_page(),'utf-8'))
+
+		elif "nth_prime" in self.path[1:]:
+			b = self.path.replace('nth_prime', '').replace('/','')
+			if b.isnumeric():
+				lst = prime(int(b))
+				self.wfile.write(bytes(str(making_list("The nth prime numbers are : ",lst)),'utf-8'))
+
+		elif "sum_of_squares" in self.path[1:]:
+			e = self.path.replace('sum_of_squares', '').replace('/','')
+			if e.isnumeric():
+				self.wfile.write(bytes(one_line_answer(f"The nth sum of squares of {e} ", recursive_sum_of_square(int(e))), 'utf-8'))
+			else:
+				self.wfile.write(bytes(error_page(), 'utf-8'))
+
+		elif "sum_of_factorial" in self.path[1:]:
+			i = self.path.replace('sum_of_factorial', '').replace('/','')
+			if i.isnumeric():
+				self.wfile.write(bytes(one_line_answer(f"The sum of nth factorial of {i} ", sum_of_factorial_recursive(int(i))), 'utf-8'))
+			else:
+				self.wfile.write(bytes(error_page(), 'utf-8'))
+
+		elif "sum_of_cubes" in self.path[1:]:
+			g = self.path.replace('sum_of_cubes', '').replace('/','')
+			if g.isnumeric():
+				self.wfile.write(bytes(one_line_answer(f"The nth sum of cubes of {g} ", recursive_sum_of_cube(int(g))), 'utf-8'))
+			else:
+				self.wfile.write(bytes(error_page(), 'utf-8'))
+
+		elif "prime" in self.path[1:]:
+			a = self.path.replace('prime', '').replace('/','')
+			if a.isnumeric():	
+				self.wfile.write(bytes(one_line_answer(f"{a} is a Prime Number ??? ", is_prime(int(a))),'utf-8'))
+			else:
+				self.wfile.write(bytes(error_page(), 'utf-8'))
+
+		elif "nth_sum" in self.path[1:]:
+			c = self.path.replace('nth_sum', '').replace('/','')
+			if c.isnumeric():
+				self.wfile.write(bytes(one_line_answer(f"The nth sum of {c} ", recursive_sum(int(c))), 'utf-8'))
+			else:
+				self.wfile.write(bytes(error_page(), 'utf-8'))
+
+		elif "square" in self.path[1:]:
+			d = self.path.replace('square', '').replace('/','')
+			if d.isnumeric():
+				self.wfile.write(bytes(one_line_answer(f"The square of {d} ", squares(int(d))), 'utf-8'))
+			else:
+				self.wfile.write(bytes(error_page(), 'utf-8'))
+
+		elif "cube" in self.path[1:]:
+			f = self.path.replace('cube', '').replace('/','')
+			if f.isnumeric():
+				self.wfile.write(bytes(one_line_answer(f"The cube of {f} ", cubes(int(f))), 'utf-8'))
+			else:
+				self.wfile.write(bytes(error_page(), 'utf-8'))
+
+		elif "factorial" in self.path[1:]:
+			h = self.path.replace('factorial', '').replace('/','')
+			if h.isnumeric():
+				self.wfile.write(bytes(one_line_answer(f"The factorial of {h} ", factorial_recursive(int(h))), 'utf-8'))
+			else:
+				self.wfile.write(bytes(error_page(), 'utf-8'))
+
+		else:
+			self.wfile.write(bytes(error_page(), 'utf-8'))
+
 
 try:
 	with HTTPServer(('localhost', 4444), handler) as server:
